22516965308e256b89183080cd9b7a79c63ea1a6
пк <pk@Mac-mini-pk.local>
2020-09-24 14:35:31 +0300

    implemented adding product to favorite

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -11,18 +11,16 @@ class ProductsGrid extends StatelessWidget {
     final products = prodcutsData.items;
 
     return GridView.builder(
-      padding: const EdgeInsets.all(10),
-      itemCount: products.length,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 1.5,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10),
-      itemBuilder: (ctx, idx) => ProductItem(
-        products[idx].id,
-        products[idx].title,
-        products[idx].imageUrl,
-      ),
-    );
+        padding: const EdgeInsets.all(10),
+        itemCount: products.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 1.5,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+        itemBuilder: (ctx, idx) => ChangeNotifierProvider.value(
+              value: products[idx],
+              child: ProductItem(),
+            ));
   }
 }
