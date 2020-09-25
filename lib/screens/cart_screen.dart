a4ce86b22171430f3599b09e032429dce1bfb688
пк <pk@Mac-mini-pk.local>
2020-09-25 16:43:58 +0300

    created drawer and order items

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/cart.dart' show Cart;
+import '../providers/orders.dart';
 import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
@@ -27,7 +28,7 @@ class CartScreen extends StatelessWidget {
                     Text('Total', style: TextStyle(fontSize: 20)),
                     Spacer(),
                     Chip(
-                      label: Text('\$${cart.totalAmount}',
+                      label: Text('\$${cart.totalAmount.toStringAsFixed(2)}',
                           style: TextStyle(
                             color: Theme.of(context)
                                 .primaryTextTheme
@@ -38,7 +39,11 @@ class CartScreen extends StatelessWidget {
                     ),
                     FlatButton(
                       child: Text('ORDER NOW'),
-                      onPressed: () {},
+                      onPressed: () {
+                        Provider.of<Orders>(context, listen: false).addOrder(
+                            cart.items.values.toList(), cart.totalAmount);
+                        cart.clear();
+                      },
                       textColor: Theme.of(context).primaryColor,
                     ),
                   ],
