1a2b5d507d6dca1c32c7947170ee4dc1254fc2f9
Dima <dima23790@gmail.com>
2021-07-19 12:29:27 +0300

    create orders provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/providers/cart.dart';
 import 'package:shop/providers/products.dart';
+import 'package:shop/providers/orders.dart';
 import 'package:shop/screens/cart_screen.dart';
 
 import 'package:shop/screens/product_detail_screen.dart';
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (ctx) => Products()),
         ChangeNotifierProvider(create: (ctx) => Cart()),
+        ChangeNotifierProvider(create: (ctx) => Orders()),
       ],
       child: MaterialApp(
         title: 'My Shop',
